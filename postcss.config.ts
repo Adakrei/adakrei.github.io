96bfaa26fe3065730b9726ad4d973fcf680c963e
carry0987 <carry0987@users.noreply.github.com>
2025-02-05 00:22:47 +0800

    Update packages

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,7 +1,5 @@
-import tailwindConfig from './tailwind.config';
-import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
-    plugins: [tailwindcss(tailwindConfig), autoprefixer()]
+    plugins: [ autoprefixer()]
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import postcss from './postcss.config';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import preact from '@preact/preset-vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [preact()],
+    plugins: [tailwindcss(), preact()],
     base: '/',
     css: {
         postcss
