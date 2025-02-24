23a1bb12181ac378909bcded1cd5b096b1c27298
carry0987 <carry0987@users.noreply.github.com>
2025-02-24 17:21:30 +0800

    Fix font-size of services section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -25,7 +25,7 @@ const service_content: object[] = [
 
 function ServiceCard({ title = 'Title', description = 'Description' }) {
     return (
-        <div className="bg-[#1E2351] text-white w-5/12 h-80 border-solid rounded-4xl grow-1 flex flex-col justify-between min-w-96">
+        <div className="bg-[#1E2351] text-white w-5/12 h-80 border-solid rounded-4xl grow-1 flex flex-col justify-between min-w-84">
             <div>
                 <h3 className="text-4xl text-left mx-6 mt-4">{title}</h3>
                 <p className="text-2xl text-left mx-6">{description}</p>
@@ -43,8 +43,8 @@ function ServiceCard({ title = 'Title', description = 'Description' }) {
 export default function Services() {
     return (
         <div className="max-w-6xl mx-auto px-4 py-16 text-white w-screen" id={'services'}>
-            <h2 className="text-4xl md:text-6xl text-left  py-3">Services</h2>
-            <div className="flex flex-wrap justify-evenly  gap-6">
+            <h2 className="text-4xl md:text-6xl text-left py-3">Services</h2>
+            <div className="flex flex-wrap justify-evenly gap-6">
                 {service_content.map((item) => (
                     <ServiceCard {...item} />
                 ))}
