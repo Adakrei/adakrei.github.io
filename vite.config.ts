7ea2b41858d38965bffc2b8210315a1be4d2cde3
carry0987 <carry0987@users.noreply.github.com>
2026-03-20 22:48:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
 export default defineConfig({
     resolve: {
